171c2e3c4831c2966a6d3261a36c46a5815581e7
Bruna <brunabarbozasofia@gmail.com>
2025-06-11 16:54:11 -0300

    responsividade do login do admin pronto

--- views/admin_login.py
+++ views/admin_login.py
@@ -3,16 +3,17 @@ import sqlite3
 
 def admin_login_view():
     email = ft.TextField(
-        label= "Digite seu email:",
-        width=800, 
-        bgcolor="#FCF8EC", 
-        color="#000000", 
+        label="Digite seu email:",
+        width=800,
+        bgcolor="#FCF8EC",
+        color="#000000",
         label_style=ft.TextStyle(size=18, weight=ft.FontWeight.W_500),
         border_radius=ft.border_radius.all(50),
         content_padding=ft.Padding(top=22, bottom=22, left=16, right=16)
     )
+
     senha = ft.TextField(
-        label= "Digite sua senha:",
+        label="Digite sua senha:",
         password=True,
         width=800,
         bgcolor="#FCF8EC",
@@ -41,102 +42,98 @@ def admin_login_view():
     return ft.View(
         route="/admin_login",
         bgcolor="#FCF8EC",
+        padding=20,
         controls=[
-            ft.Stack(
-                controls=[
-                    ft.Container(
-                        width=1700,
-                        height=850,
-                        bgcolor="white",
-                        border=ft.border.all(width=1, color="black"),
-                        shadow=ft.BoxShadow(
+            ft.Container(
+                expand=True,
+                alignment=ft.alignment.center,
+                content=ft.Column(
+                    expand=True,
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                    controls=[
+                        ft.Container(
+                            expand=True,
+                            alignment=ft.alignment.center,
+                            content=ft.ResponsiveRow(
+                                alignment=ft.MainAxisAlignment.CENTER,
+                                vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                                controls=[
+                                    ft.Container(
+                                        col={"sm": 12, "md": 10, "lg": 8, "xl": 6},
+                                        padding=40,
+                                        bgcolor="white",
+                                        border=ft.border.all(width=1, color="black"),
+                                        shadow=ft.BoxShadow(
                                             spread_radius=0,
                                             blur_radius=0,
                                             color="black",
-                                            offset=ft.Offset(30, 30)
+                                            offset=ft.Offset(15, 15)
                                         ),
-                        alignment=ft.alignment.center,
-                        content=ft.Column(
-                            controls=[
-                                ft.Column(
-                                    controls=[
-                                        ft.Container(
-                                            content=ft.Text("Login Administrador", size=32, weight="bold"),
-                                            margin=ft.margin.only(bottom=100),
-                                            alignment=ft.alignment.center
-                                        ),
-
-                                        # EMAIL
-                                        ft.Container(
-                                            content=ft.Column([
+                                        content=ft.Column(
+                                            scroll=ft.ScrollMode.AUTO,
+                                            alignment=ft.MainAxisAlignment.CENTER,
+                                            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                            controls=[
                                                 ft.Container(
-                                                    content=ft.Text("E-mail:", size=23, weight="bold"),
-                                                    alignment=ft.alignment.top_left,
-                                                    margin=ft.Margin(470, 0, 0, 0)  # left, top, right, bottom
+                                                    content=ft.Text("Login Administrador", size=32, weight="bold"),
+                                                    padding=ft.padding.only(bottom=50)
                                                 ),
-                                                ft.Container(content=email, margin=ft.margin.only(bottom=30)),
-                                            ], horizontal_alignment=ft.CrossAxisAlignment.CENTER)
-                                        ), 
-
-                                        # SENHA
-                                        ft.Container(
-                                            content=ft.Column([
+                                                ft.Column([
+                                                    ft.Container(
+                                                        content=ft.Text("E-mail:", size=23, weight="bold"),
+                                                        alignment=ft.alignment.top_left,
+                                                        padding=ft.padding.only(left=20, bottom=10)
+                                                    ),
+                                                    email,
+                                                    ft.Container(height=20)
+                                                ]),
+                                                ft.Column([
+                                                    ft.Container(
+                                                        content=ft.Text("Senha:", size=23, weight="bold"),
+                                                        alignment=ft.alignment.top_left,
+                                                        padding=ft.padding.only(left=20, bottom=10)
+                                                    ),
+                                                    senha
+                                                ]),
+                                                mensagem,
+                                                ft.Container(height=20),
                                                 ft.Container(
-                                                    content=ft.Text("Senha:", size=23, weight="bold"),
-                                                    alignment=ft.alignment.top_left,
-                                                    margin=ft.Margin(470, 0, 0, 0)  # left, top, right, bottom
+                                                    content=ft.Text("ENTRAR", size=16, weight="bold", color="black"),
+                                                    alignment=ft.alignment.center,
+                                                    width=150,
+                                                    height=50,
+                                                    bgcolor="#FCF8EC",
+                                                    border=ft.border.all(1, "black"),
+                                                    margin=ft.margin.only(top=5, left=5),
+                                                    ink=True,
+                                                    on_click=login,
+                                                    shadow=ft.BoxShadow(
+                                                        spread_radius=0,
+                                                        blur_radius=0,
+                                                        color="black",
+                                                        offset=ft.Offset(5, 5)
+                                                    )
                                                 ),
-                                                senha
-                                            ], horizontal_alignment=ft.CrossAxisAlignment.CENTER,)
-                                        ),
-
-                                        mensagem,
-
-                                        ft.Container(
-                                            content=ft.Text("ENTRAR", size=16, weight="bold", color="black"),
-                                            alignment=ft.alignment.center,
-                                            width=150,
-                                            height=50,
-                                            bgcolor="#FCF8EC",
-                                            border=ft.border.all(1, "black"),
-                                            margin=ft.margin.only(top=5, left=5),
-                                            ink=True,
-                                            on_click=login,
-                                            shadow=ft.BoxShadow(
-                                                spread_radius=0,
-                                                blur_radius=0,
-                                                color="black",
-                                                offset=ft.Offset(5, 5)
-                                            ),
-                                        ),
-                                    ],
-                                    alignment=ft.MainAxisAlignment.CENTER,
-                                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                                    expand=True
-                                ),
-                                ft.Row(
-                                    alignment=ft.MainAxisAlignment.END,
-                                    controls=[
-                                        ft.TextButton(
-                                            "← Voltar",
-                                            on_click=lambda _: _.page.go("/"),
-                                            style=ft.ButtonStyle(
-                                                color="black",
-                                            )
+                                                ft.Container(height=20),
+                                                ft.Row(
+                                                    alignment=ft.MainAxisAlignment.END,
+                                                    controls=[
+                                                        ft.TextButton(
+                                                            "← Voltar",
+                                                            on_click=lambda _: _.page.go("/"),
+                                                            style=ft.ButtonStyle(color="black")
+                                                        )
+                                                    ]
+                                                )
+                                            ]
                                         )
-                                    ]
-                                ),
-                            ],
-                            expand=True,
-                            alignment=ft.MainAxisAlignment.START,
-                            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                                    )
+                                ]
+                            )
                         )
-                    ),
-                ],
-                expand=True,
-                alignment=ft.alignment.center,
+                    ]
+                )
             )
-        ],
-        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-        vertical_alignment=ft.MainAxisAlignment.CENTER,
+        ]
     )
